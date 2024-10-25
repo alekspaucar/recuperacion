93374216298a5a8942cfad981eda9d140e1e7058
jennyfer04ramirez <jr.6311.camm@gmail.com>
2024-10-25 12:49:27 -0500

    first commit

--- src/App.java
+++ src/App.java
@@ -1,20 +1,36 @@
 
 import models.Carro;
 import models.CarrosGenerator;
+import models.Person;
+import models.PersonasGenerator;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Person[] personas = new PersonasGenerator().generarPersonas();
+        for (Person person : personas) {
+            System.out.println(person); 
+
+            MetodosOrdenamientoBusqueda mob= new MetodosOrdenamientoBusqueda();
+            mob.sortByAgeWithSelection(personas);
+            mob.showPeople(personas);
+            int result = mob.searchBinaryByAge(personas, 78);
+            if (result == - 1){
+                System.out.println("no encontro: ");
+            }else{
+                System.out.println("encontro en la posicion: " + result + "-->" + personas );
+            }
+
+        }
+    }
+}
+
+/* 
         CarrosGenerator generator = new CarrosGenerator();
-
-        // Obtener el arreglo de 50 carros generadas aleatoriamente
         Carro[] carros = generator.generarCarros();
-
         System.out.println("Listado de carros generadas:");
-
         MetodosOrdenamientoBusquedaGrupoB metodos = new MetodosOrdenamientoBusquedaGrupoB();
         metodos.sortBYearWithBubbleAvnAsendente(carros);
         metodos.showCars(carros);
-
         int result = metodos.searchBinaryByYear(carros, 200);
         if (result == -1) {
             System.out.println("No se encontro el carro");
@@ -22,4 +38,5 @@ public class App {
             System.out.println("El carro se encuentra en la posicion: " + result);
         }
     }
-}
+    */
+
--- src/MetodosOrdenamientoBusqueda.java
+++ src/MetodosOrdenamientoBusqueda.java
@@ -1,37 +1,14 @@
 
 import models.Person;
-
 /**
- * La clase MetodosOrdenamientoBusqueda proporciona métodos para ordenar y
- * buscar
- * en un arreglo de objetos Person. Los métodos de ordenamiento incluyen
- * algoritmos
- * de selección e inserción, y los métodos de búsqueda utilizan el algoritmo de
- * búsqueda binaria.
+ * MetodosOrdenamientoBusqueda
  * 
- * Métodos disponibles:
- * - sortByAgeWithSelection(Person[] people): Ordena el arreglo por edad
- * utilizando selección.
- * - sortByHeightWithSelection(Person[] people): Ordena el arreglo por altura
- * utilizando selección.
- * - sortByAgeWithInsertion(Person[] people): Ordena el arreglo por edad
- * utilizando inserción.
- * - sortByHeightWithInsertion(Person[] people): Ordena el arreglo por altura
- * utilizando inserción.
- * - searchBinaryByAge(Person[] people, int age): Realiza una búsqueda binaria
- * por edad.
- * - searchBinaryByHeight(Person[] people, int height): Realiza una búsqueda
- * binaria por altura.
- * - showPeople(Person[] people): Imprime el listado de personas en el arreglo.
+ * Aqui programar los motodos de busqueda y ordenamiento
+ * segun le tocaron en su enunciado de la evaluacion
+ * 
  */
 public class MetodosOrdenamientoBusqueda {
 
-    /**
-     * Ordena un arreglo de objetos Person por edad utilizando el algoritmo de
-     * selección.
-     *
-     * @param people El arreglo de objetos Person a ordenar.
-     */
     public void sortByAgeWithSelection(Person[] people) {
         int n = people.length;
         for (int i = 0; i < n - 1; i++) {
@@ -47,12 +24,6 @@ public class MetodosOrdenamientoBusqueda {
         }
     }
 
-    /**
-     * Ordena un arreglo de objetos Person por altura utilizando el algoritmo de
-     * selección.
-     *
-     * @param people El arreglo de objetos Person a ordenar.
-     */
     public void sortByHeightWithSelection(Person[] people) {
         int n = people.length;
         for (int i = 0; i < n - 1; i++) {
@@ -68,12 +39,6 @@ public class MetodosOrdenamientoBusqueda {
         }
     }
 
-    /**
-     * Ordena un arreglo de objetos Person por edad utilizando el algoritmo de
-     * inserción.
-     *
-     * @param people El arreglo de objetos Person a ordenar.
-     */
     public void sortByAgeWithInsertion(Person[] people) {
         int n = people.length;
         for (int i = 1; i < n; i++) {
@@ -87,12 +52,7 @@ public class MetodosOrdenamientoBusqueda {
         }
     }
 
-    /**
-     * Ordena un arreglo de objetos Person por altura utilizando el algoritmo de
-     * inserción.
-     *
-     * @param people El arreglo de objetos Person a ordenar.
-     */
+
     public void sortByHeightWithInsertion(Person[] people) {
         int n = people.length;
         for (int i = 1; i < n; i++) {
@@ -106,16 +66,7 @@ public class MetodosOrdenamientoBusqueda {
         }
     }
 
-    /**
-     * Realiza una búsqueda binaria en un arreglo de objetos Person para encontrar
-     * una edad específica.
-     *
-     * @param people El arreglo de objetos Person en el que se realizará la
-     *               búsqueda.
-     * @param age    La edad a buscar.
-     * @return El índice del objeto Person con la edad especificada, o -1 si no se
-     *         encuentra.
-     */
+
     public int searchBinaryByAge(Person[] people, int age) {
         int left = 0;
         int right = people.length - 1;
@@ -130,19 +81,10 @@ public class MetodosOrdenamientoBusqueda {
                 right = mid - 1;
             }
         }
-        return -1; // Age not found
+        return -1; 
     }
 
-    /**
-     * Realiza una búsqueda binaria en un arreglo de objetos Person para encontrar
-     * una altura específica.
-     *
-     * @param people El arreglo de objetos Person en el que se realizará la
-     *               búsqueda.
-     * @param height La altura a buscar.
-     * @return El índice del objeto Person con la altura especificada, o -1 si no se
-     *         encuentra.
-     */
+
     public int searchBinaryByHeight(Person[] people, int height) {
         int left = 0;
         int right = people.length - 1;
@@ -157,14 +99,9 @@ public class MetodosOrdenamientoBusqueda {
                 right = mid - 1;
             }
         }
-        return -1; // Height not found
+        return -1; 
     }
 
-    /**
-     * Imprime el listado de personas en el arreglo.
-     *
-     * @param people El arreglo de objetos Person a imprimir.
-     */
     public void showPeople(Person[] people) {
         for (Person person : people) {
             System.out.println(person);
